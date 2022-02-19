54d017dd2bba4d063ea70894f03b8cbd0bc1c1a2
kennedyvardo <kennedy@vardo.co>
2022-02-19 09:52:05 -0300

    rover entity

---
+++ src/entities/Rover.js
@@ -0,0 +1,9 @@
+export default class Rover {
+  constructor({ id, name, positionX, positionY, orientation }) {
+    this.id = id;
+    this.name = name;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.orientation = orientation;
+  }
+}
